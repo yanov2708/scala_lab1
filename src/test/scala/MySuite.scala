4a4bf703e79eb314556fb09aabacdd5c1f1e95a8
yanov2708 <yanov2708@gmail.com>
2022-06-24 00:07:03 +0300

    first commit

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -5,39 +5,47 @@ import munit.FunSuite
 
 class MySuite extends FunSuite {
 
-  test("map") {
-    val expected = MyList(1, 2, 3, 4, 5).map(a => a + 10)
-    val obtained = MyList(11, 12, 13, 14, 15)
-    assertEquals(obtained, expected)
+  test("map+") {
+    val obtained = MyList(1, 2, 3, 4, 5).map(a => a + 10)
+    val expected = MyList(11, 12, 13, 14, 15)
+    assertEquals(expected, obtained)
+  }
+
+  test("map*") {
+    val obtained = MyList(1, 2, 3, 4, 5).map(a => a * 5)
+    val expected = MyList(5, 10, 15, 20, 25)
+    assertEquals(expected, obtained)
   }
   
   test("reverse") {
-    val expected = MyList(1, 2, 3, 4, 5).reverse()
-    val obtained = MyList(5, 4, 3, 2, 1)
-    assertEquals(obtained, expected)
+    val obtained = MyList(1, 2, 3, 4, 5).reverse()
+    val expected = MyList(5, 4, 3, 2, 1)
+    assertEquals(expected, obtained)
+  }
+
+  test("reverseReverse") {
+    val obtained = MyList(1, 2, 3, 4, 5).reverse().reverse()
+    val expected = MyList(1, 2, 3, 4, 5)
+    assertEquals(expected, obtained)
   }
 
   test("reverseClearList") {
-    val expected = MyList().reverse()
-    val obtained = MyList()
-    assertEquals(obtained, expected)
+    val obtained = MyList().reverse()
+    val expected = MyList()
+    assertEquals(expected, obtained)
   }
 
   test("filter") {
-    val expected = MyList(1, 2, 3, 4, 5).filter(a => a < 3)
-    val obtained = MyList(1, 2)
-    assertEquals(obtained, expected)
+    val obtained = MyList(1, 2, 3, 4, 5).filter(a => a < 3)
+    val expected = MyList(1, 2)
+    assertEquals(expected, obtained)
   }
  
   test("indexOf") {
-    val expected = MyList(1, 2, 3, 4, 5).indexOf(3)
-    val obtained = Some(2)
-    assertEquals(obtained.get, expected.get)
+    val obtained = MyList(1, 2, 3, 4, 5).indexOf(3)
+    val expected = Some(2)
+    assertEquals(expected.get, obtained.get)
   }
-  test("indexOf11") {
-    val expected = MyList(1, 2, 3, 4, 5).indexOf(6)
-    val obtained = None
-    assertEquals(obtained, expected)
-  }
+
 }
 
